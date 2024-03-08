af995336a8bae7217ec2de441e342e06f1f3480c
Asubuhi824lia <der_anastasi84@mail.ru>
2024-03-08 20:53:23 +0700

    add: typed context

--- src/components/SearchResults/SearchContext.ts
+++ src/components/SearchResults/SearchContext.ts
@@ -1,3 +1,21 @@
 import { createContext } from "react";
 
-export const SearchContext = createContext({ users: [] });
+export type UserAddressType = {
+  city: string;
+};
+export type UserType = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  image: string;
+  address: UserAddressType;
+};
+export type SearchContextType = {
+  users: UserType[];
+  setUsers: (users: UserType[]) => void;
+};
+
+export const SearchContext = createContext<SearchContextType>({
+  users: [],
+  setUsers: () => {},
+});
