f109207e63e3e0284230048c061d5dbbdb27fb7b
Asubuhi824lia <der_anastasi84@mail.ru>
2024-03-09 21:11:45 +0700

    add: img lazy-loading

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,10 +1,13 @@
+import LazyLoad from "react-lazy-load";
 import { UserType } from "../SearchResults/SearchContext";
 import "./style.css";
 
 export function UserCard(props: UserType) {
   return (
     <div className="userCard">
-      <img className="userPic" src={props.image} />
+      <LazyLoad height={80} width={80} threshold={0.55}>
+        <img className="userPic" src={props.image} />
+      </LazyLoad>
       <div className="userInfo">
         <div>{`${props.firstName} ${props.lastName}`}</div>
         <div>{props.address.city}</div>
